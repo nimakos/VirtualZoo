4bddac912e3d4508344d429f43dfb60435129686
nimakos <nimakos@hotmail.com>
2021-08-30 16:27:24 +0300

    add async threads

--- web/src/main/java/gr/nikolis/WebMain.java
+++ web/src/main/java/gr/nikolis/WebMain.java
@@ -5,11 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableScheduling
-public class WebMain implements CommandLineRunner {
+@EnableAsync
+public class WebMain implements CommandLineRunner, AsyncConfigurer {
 
     @Autowired
     private DBSeeders dbSeeders;
@@ -21,5 +27,16 @@ public class WebMain implements CommandLineRunner {
     @Override
     public void run(String... args) {
         dbSeeders.run();
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(10);
+        executor.setThreadNamePrefix("DemoExecutor-");
+        executor.initialize();
+        return executor;
     }
 }
--- web/src/main/java/gr/nikolis/controller/AnimalController.java
+++ web/src/main/java/gr/nikolis/controller/AnimalController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.WebAsyncTask;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,27 +23,35 @@ public class AnimalController {
     private AnimalService animalService;
 
     @RequestMapping(value = "/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Animal> getAnimals() {
-        animalService.fillSpeciesList();
-        return new ArrayList<>(animalService.findAll());
+    public WebAsyncTask<List<Animal>> getAnimals() {
+        return new WebAsyncTask<>(4000, () -> {
+            animalService.fillSpeciesList();
+            return new ArrayList<>(animalService.findAll());
+        });
     }
 
     @RequestMapping(value = "/group", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<String> getGroupedAnimals() {
-        List<Animal> animalList = new ArrayList<>(animalService.findAll());
-        Map<String, List<Animal>> animalListGrouped = animalList.stream().collect(Collectors.groupingBy(Animal::getSpecie));
-        return new ArrayList<>(animalListGrouped.keySet());
+    public WebAsyncTask<List<String>> getGroupedAnimals() {
+        return new WebAsyncTask<>(4000, () -> {
+            List<Animal> animalList = new ArrayList<>(animalService.findAll());
+            Map<String, List<Animal>> animalListGrouped = animalList.stream().collect(Collectors.groupingBy(Animal::getSpecie));
+            return new ArrayList<>(animalListGrouped.keySet());
+        });
     }
 
     @RequestMapping(value = "/{animalId}/doTrick", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String getTrick(@PathVariable(value = "animalId") long animalId) {
-        String animalTrick = animalService.animalRandomTrick(animalId);
-        return "{ \"trick\": \"" + animalTrick + "\"}";
+    public WebAsyncTask<String> getTrick(@PathVariable(value = "animalId") long animalId) {
+        return new WebAsyncTask<>(4000, () -> {
+            String animalTrick = animalService.animalRandomTrick(animalId);
+            return "{ \"trick\": \"" + animalTrick + "\"}";
+        });
     }
 
     @RequestMapping(value = "/{animalId}/learnTrick", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String learnTrick(@PathVariable(value = "animalId") long animalId) {
-        String learnedTrick = animalService.learnTrick(animalId);
-        return "{ \"learnedTrick\": \"" + learnedTrick + "\"}";
+    public WebAsyncTask<String> learnTrick(@PathVariable(value = "animalId") long animalId) {
+        return new WebAsyncTask<>(4000, () -> {
+            String learnedTrick = animalService.learnTrick(animalId);
+            return "{ \"learnedTrick\": \"" + learnedTrick + "\"}";
+        });
     }
 }
