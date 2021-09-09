b108908ea808e2f7d6ee2c10698c7eac373f9a14
nimakos <nimakos@hotmail.com>
2021-09-09 10:02:06 +0300

    networking

--- web/src/test/java/gr/nikolis/TestApplication.java
+++ web/src/test/java/gr/nikolis/TestApplication.java
@@ -7,16 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class TestApplication {
     public static final String URL = "https://localhost:8888/myZoo/animals/all";
 
-    /*@Autowired
-    private JsonParser jsonParser;*/
-
     @Test
     public void testing() {
-/*        try {
-            List<Animal> data = jsonParser.toList(Animal.class, URL, "");
-            System.out.println(data);
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }*/
+
     }
 }
